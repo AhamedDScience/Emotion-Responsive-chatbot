a89f2c63f86bea5c2bd4ebd167d9095e57e305f7
AhamedDScience <167436292+AhamedDScience@users.noreply.github.com>
2024-05-24 18:04:50 +0530

    streamlit.py

--- app/streamlit.py
+++ app/streamlit.py
@@ -7,18 +7,15 @@ def main():
     video_file = st.file_uploader("Upload Video", type=["mp4", "mov"])
     
     if video_file is not None:
-        st.text("Video uploaded successfully!")
         
         # Show loading message while processing
-        with st.spinner('Processing video...'):
+        with st.spinner():
             # Call function to process video and get audio
             val = process_camera(video_file.name)
             audio_file = val[0]
-        st.success("Video processed successfully!")
         
         # Display audio file and provide option to play it
         st.audio(audio_file, format='audio/wav', start_time=0)
-        st.success("Audio file ready!")
         
 
 main()
